be6633f93ed66bd5e7c7b8b5fcbebb949008bc93
ssadevtech <ssadevtech@gmail.com>
2021-02-08 13:52:33 +0000

    Create nmtenes project

---
+++ nmtenes/get_files.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [ "$1" = "src" ]; then
+    rm -f nmt_model.py nmt_utils.py nmt_vocab.py run_nmt_enes.py
+    cp ../../../src/{nmt_model,nmt_utils,nmt_vocab,run_nmt_enes}* .
+
+elif [ "$1" = "data" ]; then
+    mkdir data
+    cp ../../../datasets/en_es_data/{dev,test,train}* ./data/.
+
+else
+    echo "Invalid option"
+    echo {nmt_model, nmt_utils, nmt_vocab, run_nmt_enes}.py
+fi
